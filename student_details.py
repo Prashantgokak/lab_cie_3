2216a5cf7d14321b97076698f8e877679849101b
Prashantgokak <prashantgokak500@gmail.com>
2025-12-27 12:32:53 +0530

    saving

---
+++ student_details.py
@@ -0,0 +1,11 @@
+def student_details(usn, name,div,age):
+    result= (
+        student USN ="{usn}";
+        student NAME ="{name}";
+        student DIVION ="{div}";
+        student AGE ="{age}";
+    )
+    return result 
+if __name__main__
+print(student_details(302,"akash","E",19))
+
