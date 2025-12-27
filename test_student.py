d7fb747d9b18adccc12ad07c6d4d29d86a10d6f3
Prashantgokak <prashantgokak500@gmail.com>
2025-12-27 13:37:58 +0530

    Update and rename test_student_details.py to test_student.py

--- test_student.py
+++ test_student.py
@@ -1,4 +1,4 @@
-from student_details import test_student_details
+from student import test_student_details
 test_student_details():
 expected_output = {
     "student_usn": "302",
@@ -8,6 +8,7 @@ expected_output = {
 }
 
 assert tstudent_details(302, "akash", "E", 19) == expected_output
+
 
 
 
