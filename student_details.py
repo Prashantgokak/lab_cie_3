c71cd5df37ad5eaf1c1b9ac4df0be112394c8cd9
Prashantgokak <prashantgokak500@gmail.com>
2025-12-27 13:05:41 +0530

    Fix formatting and string interpolation in student_details

--- student_details.py
+++ student_details.py
@@ -1,11 +1,11 @@
-def student_details(usn, name,div,age):
-    result= (
-        student USN ="{usn}";
-        student NAME ="{name}";
-        student DIVION ="{div}";
-        student AGE ="{age}";
+def student_details(usn, name, div, age):
+    result = (
+        f"Student USN = {usn}\n"
+        f"Student Name = {name}\n"
+        f"Student Division = {div}\n"
+        f"Student Age = {age}"
     )
-    return result 
-if __name__ == "__main__":
- print(student_details(302,"akash","E",19))
+    return result
 
+if __name__ == "__main__":
+    print(student_details(302, "akash", "E", 19))
